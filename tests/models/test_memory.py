0e140ffdd75025da309ef2e21a92ca4239f07606
Grant Dempsey <CryptoDemps@gmail.com>
2021-02-22 17:15:59 -0700

    Model test for memory
    Co-Authored-By: Eduardo Parra <helloeduardo@users.noreply.github.com>
    Co-Authored-By: Sean Steel <s-steel@users.noreply.github.com>
    Co-Authored-By: Jesse Mellinger <jesse.m.mellinger@gmail.com>

---
+++ tests/models/test_memory.py
@@ -0,0 +1,36 @@
+import unittest
+from sqlalchemy.exc import IntegrityError
+
+from api import create_app, db
+from api.database.models import User, Room, Memory
+
+
+class AppTest(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app('testing')
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+        self.client = self.app.test_client()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+
+    def test_memory_model(self):
+        user = User(name='ian', email='ian.douglas@iandouglas.com')
+        user.insert()
+        room = Room(name='Kitchen', image='exampleimage1.com', user_id=user.id)
+        room.insert()
+        memory = Memory(image='Picture string', song='song url', description='This is a great memory', aromas='Roast in the oven', location='table', room_id=room.id)
+        memory.insert()
+
+        self.assertIsInstance(memory, Memory)
+        self.assertIsNotNone(memory.id)
+        self.assertEqual('Picture string', memory.image)
+        self.assertEqual('song url', memory.song)
+        self.assertEqual('This is a great memory', memory.description)
+        self.assertEqual('Roast in the oven', memory.aromas)
+        self.assertEqual('table', memory.location)
+        self.assertEqual(room.id, memory.room_id)
