a48fcfbb3f12d11e6c096536066cd46816f5d3d9
Sean Steel <seanrsteel@gmail.com>
2021-02-24 15:39:28 -0700

    Finish setting up test for the room by id endpoint
    Co-authored-by: Eduardo Parra <56360157+helloeduardo@users.noreply.github.com>
    Co-authored-by: Grant Dempsey <34411972+GDemps@users.noreply.github.com>
    Co-authored-by: Jesse Mellinger <29828129+JesseMellinger@users.noreply.github.com>

--- tests/endpoints/rooms/test_get_one_room.py
+++ tests/endpoints/rooms/test_get_one_room.py
@@ -14,12 +14,35 @@ class GetRoomTest(unittest.TestCase):
         db.create_all()
         self.client = self.app.test_client()
 
-        self.user_1 = User(name='zzz 1', email='e1')
-        self.user_1.insert()
-
     def tearDown(self):
         db.session.remove()
         db_drop_everything(db)
         self.app_context.pop()
 
-    def test_happypath_get_a_user(self):
+    def test_happypath_get_a_room(self):
+      user_1 = User(name='Test User', email='email 1')
+      user_1.insert()
+      room_1 = Room(name='Kitchen', image='exampleimage1.com', user_id=user_1.id)
+      room_1.insert()
+      room_2 = Room(name='Living Room', image='exampleimage2.com', user_id=user_1.id)
+      room_2.insert()
+
+      response = self.client.get(
+      f'/api/v1/rooms/{room_2.id}'
+      )
+      self.assertEqual(200, response.status_code)
+
+      data = json.loads(response.data.decode('utf-8'))
+      assert_payload_field_type_value(self, data, 'success', bool, True)
+      
+      results = data['data']
+
+      assert_payload_field_type_value(
+            self, results, 'name', str, self.room_2.name
+        )
+      assert_payload_field_type_value(
+          self, results, 'image', str, self.room_2.image
+      )
+      assert_payload_field_type_value(
+          self, results, 'user_id', str, self.user_1.id
+      )
