84aa8b85a18ae0b8eb166f81c6dc3cf74510c9bc
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2021-02-25 17:41:52 -0700

    Add psql file to create database and python file for python commands
    Co-Authored-By: Eduardo Parra <helloeduardo@users.noreply.github.com>
    Co-Authored-By: Grant Dempsey <GDemps@users.noreply.github.com>
    Co-Authored-By: Sean Steel <s-steel@users.noreply.github.com>

---
+++ psql.sh
@@ -0,0 +1,2 @@
+"psql -c 'create database untangled_dev;' -U postgres"
+"psql --username=username --dbname=untangled_dev"
---
+++ python.py
@@ -0,0 +1,6 @@
+source venv/bin/activate
+python3 manage.py db upgrade
+python3 manage.py db_seed
+python3 run.py
+
+
